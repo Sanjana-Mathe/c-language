4ff74f222781ce2d8dfeffd74d3534a1e70e9c94
Sanjana-Mathe <sanjanamathe02@gmail.com>
2025-08-01 09:49:41 +0530

    Add files via upload

---
+++ Relational.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int a=50;
+	int b=100;
+	int c;
+	c=a==b;
+	printf("a==b is %d\n",c);
+	c=a!=b;
+	printf("a!=b is %d\n",c);
+	c=a<b;
+	printf("a<b is %d\n",c);
+	c=a>b;
+	printf("a>b is %d\n",c);
+	c=a<=b;
+	printf("a<=b is %d\n",c);
+	c=a>=b;
+	printf("a>=b is %d\n",c);
+}
