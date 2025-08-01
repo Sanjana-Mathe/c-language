5a34fcacce68b32d0612d17c4f0b4b8b2ded9835
Sanjana-Mathe <sanjanamathe02@gmail.com>
2025-08-01 09:54:29 +0530

    Add files via upload

---
+++ if else.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a=90;
+	if(a>0){
+	printf("number is positive");
+}else if (a<0){
+	printf("number is negative");
+}else if(a==0){
+	printf("number is neutral");
+}
+}
+
+	
