ced5a8c782e0af395a7558211c4411a2e5354be9
Sanjana-Mathe <sanjanamathe02@gmail.com>
2025-08-01 10:03:07 +0530

    Add files via upload

---
+++ no.btn 1to7.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main(){
+	int choise;
+	printf("enter number between 1 to 7\n");
+	scanf("%d",&choise);
+		switch(choise){
+		case 1:printf("monday");
+		break;
+		case 2:printf("tuesday");
+		break;
+		case 3:printf("wednesday");
+		break;
+		case 4:printf("thursday");
+		break;
+		case 5:printf("friday");
+		break;
+		case 6:printf("saturday");
+		break;
+	    case 7:printf("sunday");
+		break;
+		default: printf("invalid input");
+	}
+}
