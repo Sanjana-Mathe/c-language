c95db793cdb950611b48f2b7b3d461674353a473
Sanjana-Mathe <sanjanamathe02@gmail.com>
2025-08-01 09:52:06 +0530

    Add files via upload

---
+++ logical.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int x=10;
+	int y=10;
+	int z;
+	z=x+y;
+	printf("x+y is %d\n",z);
+	z=x&y;
+	printf("x&y is %d\n",z);
+	z=x|y;
+	printf("x|y is %d\n",z);
+	z=x^y;
+	printf("x^y is %d\n",z);
+}
