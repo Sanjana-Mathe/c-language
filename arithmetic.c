c7dbfd3b03af82b980d1b15597c109bf3467f46b
Sanjana-Mathe <sanjanamathe02@gmail.com>
2025-08-01 09:44:23 +0530

    Add files via upload
    This file content arithmetic operation practices done by me during my certification course

---
+++ arithmetic.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int a=10;
+	int b=20;
+	int c;
+	c=a+b;
+	printf("a+b is %d\n",c);
+	c=a-b;
+	printf("a-b is %d\n",c);
+	c=a*b;
+	printf("a*b is %d\n",c);
+	c=a/b;
+	printf("a/b is %d\n",c);
+	c=a%b;
+	printf("a%b is %d\n",c);
+}
